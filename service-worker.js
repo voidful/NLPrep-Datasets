dcd8eecfce351d585e96fc3853e2337bfe77e26e
voidful <voidful@users.noreply.github.com>
2020-07-13 17:06:06 +0000

    deploy: 19bef34afd5036e432302fac2f99213459528c4c

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6faee963c771746eb90ddbbb5646a827.js"
+  "/NLPrep-Datasets/precache-manifest.7aa5bb22bd68869708d61d1de633c9cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NLPrep-Datasets/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
