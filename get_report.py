5db8387f8bbd73c94638fcff1376fc2fbc3cbca1
voidful <voidful.stack@gmail.com>
2020-07-13 21:42:06 +0800

    get_report get_dir_with_notexist_create

--- get_report.py
+++ get_report.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+import nlp2
 import nlprep
 
 datasets_json = {}
@@ -10,7 +10,8 @@ for dataset in nlprep.list_all_datasets():
     for ds_name, mf in nlprep.convert_middleformat(ds).items():
         print(ds_name, ds_info, mf.dump_list()[:3])
         profile = mf.get_report(ds_name)
-        profile.to_file(os.path.join('./website/public/reports/', ds_name + "_report.html"))
+        profile.to_file(
+            os.path.join(nlp2.get_dir_with_notexist_create('./website/public/reports/'), ds_name + "_report.html"))
         datasets_json[dataset + " : " + ds_name] = {
             "dstag": dataset,
             "task": ds_info['TASK'],
@@ -19,5 +20,7 @@ for dataset in nlprep.list_all_datasets():
             "ref": ds_info['REF'],
             "files": list(ds_info['DATASET_FILE_MAP'].keys())
         }
-    with open('./website/src/assets/datasets.json', 'w', encoding='utf8') as f:
+
+    with open(os.path.join(nlp2.get_dir_with_notexist_create('./website/src/assets/'), 'datasets.json'), 'w',
+              encoding='utf8') as f:
         json.dump(datasets_json, f, ensure_ascii=False)
